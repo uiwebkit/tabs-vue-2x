4e818b73987fe96f9fa4afaa77789ea237890a4f
UiWebKit <uniwebkit@gmail.com>
2022-01-17 18:59:56 +0200

    init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.config.ignoredElements = [/uni-\w*/];
 
 new Vue({
   render: h => h(App),
